5cf1809666f4bc4ab351c54978df358a68310b95
Sodiq 'Ade' Sanusi <ademilddiq@gmail.com>
2024-07-12 00:17:28 +0100

    feat(mobile): create mobile header and styling

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,11 +1,23 @@
 import styles from "./Header.module.css";
+import Image from "next/image";
+import { FaBars } from "react-icons/fa6";
+import logo from "../../public/images/logo.svg";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <header>
-      This is the Header Section
+    <header className={styles.container}>
+      <div className={styles.imageLinkContainer}>
+        <Link href="/">
+          <Image src={logo} alt="The logo of Our Common Pot"/>
+        </Link>
+      </div>
+
+      <button className={styles.hamburgerButton}>
+        <FaBars size={24}/>
+      </button>
     </header>
   );
 }
- 
+
 export default Header;
