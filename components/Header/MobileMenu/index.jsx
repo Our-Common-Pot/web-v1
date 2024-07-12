041ca0e013efbff759b00a711bd73fda8aaeb94d
Sodiq 'Ade' Sanusi <ademilddiq@gmail.com>
2024-07-12 02:05:48 +0100

    feat(mobile): style mobile menu alongside functionalities

---
+++ components/Header/MobileMenu/index.jsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../../../public/images/logo_contrast.svg";
+import cancel_menu from "../../../public/images/cancel_mobilemenu.svg";
+import parentStyles from "../Header.module.css";
+import styles from "./MobileMenu.module.css";
+
+const MobileMenu = ({handleMobileMenuClick}) => {
+  return (
+    <main className={styles.layout}>
+      <div className={parentStyles.container}>
+        <div className={parentStyles.imageLinkContainer}>
+          <Link href="/" onClick={handleMobileMenuClick}>
+            <Image alt="Our Common Pot's Logo" src={logo}/>
+          </Link>
+        </div>
+
+        <button 
+          className={parentStyles.hamburgerButton}
+          onClick={handleMobileMenuClick}
+        >
+          <Image src={cancel_menu} alt="Click to close the mobile menu"/>
+        </button>
+      </div>
+
+      <ul className={styles.linkContainer}>
+        <li className={styles.linkItem}>
+          <Link href="/" onClick={handleMobileMenuClick}>Home</Link>
+        </li>
+        <li className={styles.linkItem}>
+          <Link href="/about" onClick={handleMobileMenuClick}>About Us</Link>
+        </li>
+        <li className={styles.linkItem}>
+          <Link href="/contact" onClick={handleMobileMenuClick}>Contact Us</Link>
+        </li>
+        <li className={styles.linkItem}>
+          <Link href="/donate" onClick={handleMobileMenuClick}>Donations</Link>
+        </li>
+        <li className={styles.linkItem}>
+          <Link href="/testimonials" onClick={handleMobileMenuClick}>Media</Link>
+        </li>
+      </ul>
+    </main>
+  );
+}
+ 
+export default MobileMenu;
--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -3,8 +3,17 @@ import Image from "next/image";
 import { FaBars } from "react-icons/fa6";
 import logo from "../../public/images/logo.svg";
 import Link from "next/link";
+import MobileMenu from "./MobileMenu";
+import { useState } from "react";
 
 const Header = () => {
+  const [mobileMenuOpen, setMobileMenuState] = useState(false);
+
+  const handleMobileMenuClick = () => {
+    setMobileMenuState(!mobileMenuOpen);
+  }
+
+
   return (
     <header className={styles.container}>
       <div className={styles.imageLinkContainer}>
@@ -13,9 +22,14 @@ const Header = () => {
         </Link>
       </div>
 
-      <button className={styles.hamburgerButton}>
+      <button 
+        className={styles.hamburgerButton}
+        onClick={handleMobileMenuClick}
+      >
         <FaBars size={24}/>
       </button>
+
+      {mobileMenuOpen && <MobileMenu handleMobileMenuClick={handleMobileMenuClick}/>}
     </header>
   );
 }
